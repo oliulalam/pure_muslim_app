5e6dbe69df17b36522191f5dd833b4548d14f668
oliulalam <mdoliulalamjoy@gmail.com>
2026-01-19 20:09:50 +0600

    Added Home Page Appbar

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,7 +11,22 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        backgroundColor: Color(0xFF044B77),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Pure Muslim", style: TextStyle(color: Colors.white),),
+            Row(
+              children: [
+                Icon(Icons.location_on, color: Colors.white,),
+                SizedBox(width: 5,),
+                Text("Dhaka, Bangladesh", style: TextStyle(fontSize: 18, color: Colors.white),),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
