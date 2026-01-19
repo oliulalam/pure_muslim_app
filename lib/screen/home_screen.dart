3b8c10a7470de4d58743891b856fe189518fa60e
oliulalam <mdoliulalamjoy@gmail.com>
2026-01-19 22:31:19 +0600

    Added Home Page Date And Time Scetion

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -8,24 +8,104 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+
+
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFF044B77),
         title: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text("Pure Muslim", style: TextStyle(color: Colors.white),),
-            Row(
-              children: [
-                Icon(Icons.location_on, color: Colors.white,),
-                SizedBox(width: 5,),
-                Text("Dhaka, Bangladesh", style: TextStyle(fontSize: 18, color: Colors.white),),
-              ],
+            Text(
+              "Pure Muslim",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontFamily: 'Poppins',
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Location Change");
+              },
+              child: Row(
+                children: [
+                  Icon(Icons.location_on, color: Colors.white),
+                  SizedBox(width: 5),
+                  Text(
+                    "Dhaka, Bangladesh",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
+      ),
+
+      body: Column(
+        children: [
+          Container(
+            height: screenHeight * 0.27,
+            decoration: BoxDecoration(
+              color: Color(0xFF044B77),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            width: double.infinity,
+
+            child: Padding(
+              padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '17-Jan-2026 | Saturday',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    '3 Magh 1433 | Winter',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    '28 Rajab 1447 AH',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontSize: 14,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
