db858309933efe9f94fc6b483c98ff28fbfef388
oliulalam <mdoliulalamjoy@gmail.com>
2026-01-23 23:14:13 +0600

    Added Prayaer Time Model

--- lib/Model/PrayerTimeModel.dart
+++ lib/Model/PrayerTimeModel.dart
@@ -1,35 +1,39 @@
+import 'package:intl/intl.dart';
+
 class PrayerTimeModel {
   final String fajr;
+  final String sunrise;
   final String dhuhr;
   final String asr;
   final String maghrib;
   final String isha;
-  final String readableDate;
-  final String hijriDate;
 
   PrayerTimeModel({
     required this.fajr,
+    required this.sunrise,
     required this.dhuhr,
     required this.asr,
     required this.maghrib,
     required this.isha,
-    required this.readableDate,
-    required this.hijriDate,
   });
 
   factory PrayerTimeModel.fromJson(Map<String, dynamic> json) {
-    final data = json['data'];
-    final timings = data['timings'];
-    final date = data['date'];
+    final timings = json['timings'];
+
+    // সময় কনভার্ট করার ফাংশন
+    String convertTo12Hour(String time) {
+      // API থেকে আসা সময় সাধারণত "HH:mm" ফরম্যাটে থাকে
+      DateTime tempDate = DateFormat("HH:mm").parse(time);
+      return DateFormat("hh:mm a").format(tempDate); // এটি "05:30 PM" ফরম্যাটে দিবে
+    }
 
     return PrayerTimeModel(
-      fajr: timings['Fajr'],
-      dhuhr: timings['Dhuhr'],
-      asr: timings['Asr'],
-      maghrib: timings['Maghrib'],
-      isha: timings['Isha'],
-      readableDate: date['readable'],
-      hijriDate: date['hijri']['date'],
+      fajr: convertTo12Hour(timings['Fajr']),
+      sunrise: convertTo12Hour(timings['Sunrise']),
+      dhuhr: convertTo12Hour(timings['Dhuhr']),
+      asr: convertTo12Hour(timings['Asr']),
+      maghrib: convertTo12Hour(timings['Maghrib']),
+      isha: convertTo12Hour(timings['Isha']),
     );
   }
 }
